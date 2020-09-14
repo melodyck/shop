b4222cad6656dbe0d052668363836ff5a3b659cd
melodyck <1315782936@qq.com>
2020-09-14 14:32:59 +0800

    add package

--- src/main/java/web/servlet/BaseServlet.java
+++ src/main/java/web/servlet/BaseServlet.java
@@ -13,25 +13,31 @@ import java.lang.reflect.Method;
 public class BaseServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String uri=req.getRequestURI();
-        String method=uri.substring(uri.lastIndexOf("/")+1);
+        //从uri(文件地址)中截取方法名
+        String uri = req.getRequestURI();
+        String methodName = uri.substring(uri.lastIndexOf("/") + 1);
+        System.out.println(methodName);
+
+        //this表示调用service方法的servlet对象
+        //通过this.getClass()获取字节码对象
+        Class<? extends BaseServlet> cls = this.getClass();
         try {
-            Method method1 = this.getClass().getMethod(method,HttpServletRequest.class,HttpServletResponse.class);
-            method1.invoke(this,req,resp);
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
+            //通过字节码对象和方法名获取对应方法
+            Method method = cls.getMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
+            //执行方法
+            method.invoke(this, req, resp);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
     }
-    public void writeVaule(HttpServletResponse response, Object info){
-        ObjectMapper mapper = new ObjectMapper();
+
+    //在响应中封装json对象(提取重复语句)
+    public void outputJson(HttpServletRequest request, HttpServletResponse response, Object info) {
         response.setContentType("application/json;charset=utf-8");
+        ObjectMapper mapper = new ObjectMapper();
         try {
-            mapper.writeValue(response.getOutputStream(),info);
-        } catch (IOException e) {
+            mapper.writeValue(response.getOutputStream(), info);
+        }catch (IOException e){
             e.printStackTrace();
         }
     }
